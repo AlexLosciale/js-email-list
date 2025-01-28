358de7f981a2e13466f46b813fd486fc97d23320
AlexLosciale <losciale17@gmail.com>
2025-01-28 16:53:04 +0100

    add:fetch

--- main.js
+++ main.js
@@ -2,12 +2,16 @@
 Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
 */
 
-fetch('https://flynn.boolean.careers/exercises/api/random/mail')
-.then(response => response.json())
-.then(data => {
-    console.log(data.response);
-    for (let i = 0; i < 10; i++) {
-        document.getElementById('mail-list').innerHTML += `<li>${data.response}</li>`;
-    }
-})
-.catch(error => console.error(error));
+const emailList = document.getElementById("mail-list");
+const apiURL = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+for (let i = 0; i < 10; i++) {
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            const listItem = document.createElement("li");
+            listItem.textContent = data.response;
+            emailList.appendChild(listItem);
+        })
+        .catch(error => console.error("Errore durante il recupero delle email:"));
+};
